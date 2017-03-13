2243ab20d4bf644aeb7f55957e7e95cb8ad8f3cd
Chery.Peng <chery.peng@duitang.com>
2017-03-13 16:40:53 +0800

    update htmltestrunner

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by Pcc on 3/13/10
+
 import sys
 import unittest
-import HtmlTestRunner
+import HTMLTestRunner
+import datetime
 sys.path.append('./api_tests')
 
 
@@ -10,9 +15,16 @@ discover = unittest.defaultTestLoader.discover(test_dir, pattern='*_test.py')
 
 if __name__ == '__main__':
     # 如果需要html格式输出测试报告,可用下面的html_runner
-    # html_runner = HtmlTestRunner.HTMLTestRunner(output='../../api_test_results', verbosity=2)
-    # html_runner.run(discover)
+    str_time = datetime.datetime.now().strftime('%b_%d_%y_%H_%M_%S')
+    fp = file('../api_test_report_' + str_time + '.html', 'wb')
+    runner = HTMLTestRunner.HTMLTestRunner(
+        stream=fp,
+        title='Api_Test_Report',
+        description='This demonstrates the report output by HTMLTestRunner.',
+        verbosity=2
+    )
+    runner.run(discover)
 
     # 普通报告输出
-    runner = unittest.TextTestRunner(verbosity=2)
-    runner.run(discover)
+    # runner = unittest.TextTestRunner(verbosity=2)
+    # runner.run(discover)
