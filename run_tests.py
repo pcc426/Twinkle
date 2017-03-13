0827b97a92f95dd0c29887334fbd986cab038032
Chery.Peng <chery.peng@duitang.com>
2017-03-13 15:58:30 +0800

    add html_runner,tv_category_test

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,6 @@
 import sys
 import unittest
+import HtmlTestRunner
 sys.path.append('./api_tests')
 
 
@@ -8,5 +9,10 @@ test_dir = './api_tests'
 discover = unittest.defaultTestLoader.discover(test_dir, pattern='*_test.py')
 
 if __name__ == '__main__':
-    runner = unittest.TextTestRunner()
+    # 如果需要html格式输出测试报告,可用下面的html_runner
+    # html_runner = HtmlTestRunner.HTMLTestRunner(output='../../api_test_results', verbosity=2)
+    # html_runner.run(discover)
+
+    # 普通报告输出
+    runner = unittest.TextTestRunner(verbosity=2)
     runner.run(discover)
